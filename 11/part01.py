a84e65facaddceebe1b50fbbe4fa056ec92ab640
Brenton Cleeland <brenton@brntn.me>
2022-12-11 19:25:55 +1100

    optimise :)

--- 11/part01.py
+++ 11/part01.py
@@ -7,41 +7,41 @@ class Monkey:
 
     def __init__(self, num, items, operation, test, if_true, if_false):
         self.num = num
-        self.items = [int(x) for x in items.split(",")]
+        self.items = [mpz(x) for x in items.split(",")]
         self.operation = operation
         self.test = test
         self.if_true_dest = int(if_true.split()[-1])
         self.if_false_dest = int(if_false.split()[-1])
         self.inspections = 0
+        self.op, self.num = self.operation.split()[-2:]
+
+        self.use_old = self.num == 'old'
+
+        if not self.use_old:
+            self.num = mpz(int(self.num))
 
     def test_item(self, item, round=""):
         if self.test.startswith("divisible by"):
             x = int(self.test.split()[-1])
             t = time.time()
-            print("doing mod")
             result = item % x == 0
-            print(f"done mod {time.time() - t} {round}")
             return result
         else:
             print("Did not parse test")
 
     def inspect_item(self, item):
-        print('a')
         self.inspections += 1
-        op, num = self.operation.split()[-2:]
-        print('b')
-        if num == 'old':
-            num = item
 
-        if op == '+':
-            print('c')
-            result = int(num) + item
-            print('d')
+        if self.use_old:
+            num = item
+        else:
+            num = self.num
+
+        if self.op == '+':
+            result = num + item
             return result
-        elif op == '*':
-            print('e')
-            result = int(num) * mpz(item)
-            print('f')
+        elif self.op == '*':
+            result = num * item
             return result
         else:
             print("Unexpected operation")
@@ -61,11 +61,9 @@ def puzzle_func(fn, rounds, use_relief=True):
             print(round)
             for monkey in monkeys:
                 while monkey.items:
-                    print('1')
                     initial = monkey.items.pop(0)
 
                     new = monkey.inspect_item(initial)
-                    print('2')
 
                     # relief?!
                     if use_relief:
@@ -77,7 +75,6 @@ def puzzle_func(fn, rounds, use_relief=True):
                         monkeys[monkey.if_true_dest].items.append(i)
                     else:
                         monkeys[monkey.if_false_dest].items.append(i)
-                    print('3')
 
     sorted_inspections = sorted([m.inspections for m in monkeys], reverse=True)[:2]
     print(sorted_inspections)
