072aefaef77781eef97c2533b0b958166dc8939a
Brenton Cleeland <brenton@brntn.me>
2022-12-11 19:27:42 +1100

    use 10k

--- 11/part02.py
+++ 11/part02.py
@@ -2,10 +2,10 @@ from part01 import puzzle_func
 
 
 if __name__ == "__main__":
-    result = puzzle_func("test.txt", 1000, use_relief=False)
+    result = puzzle_func("test.txt", 10000, use_relief=False)
     print(result)
     assert result == 2713310158
 
-    result = puzzle_func("input.txt", 1000, use_relief=False)
+    result = puzzle_func("input.txt", 10000, use_relief=False)
     print(result)
 
