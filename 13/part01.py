13e7bcf8b2c7d25e46f4d723e97f6efe1857c194
Brenton Cleeland <brenton@brntn.me>
2022-12-13 17:28:20 +1100

    on the thirteenth day it's all __lt__

---
+++ 13/part01.py
@@ -0,0 +1,56 @@
+import json
+
+def compare(left, right):
+    if type(left) == int and type(right) == list:
+        left = [left]
+    if type(right) == int and type(left) == list:
+        right = [right]
+
+    if type(left) == int and type(right) == int:
+        # If both values are integers, the lower integer should come first.
+        # If the left integer is lower than the right integer, the inputs are in the right order.
+        # If the left integer is higher than the right integer, the inputs are not in the right order.
+        # Otherwise, the inputs are the same integer; continue checking the next part of the input.
+        if left < right:
+            return True
+        if left > right:
+            return False
+    else:
+        # If both values are lists, compare the first value of each list, then the second value, and so on.
+        # If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+        for i, l in enumerate(left):
+            # If the right list runs out of items first, the inputs are not in the right order.
+            try:
+                r = right[i]
+            except IndexError:
+                return False
+
+            result = compare(l, r)
+            if result != None:
+                return result
+
+        # If the left list runs out of items first, the inputs are in the right order.
+        if len(left) < len(right):
+            return True
+
+
+def find_ordered_pairs(fn):
+    with open(fn) as f:
+        pairs = [[json.loads(y) for y in x.splitlines()] for x in f.read().split("\n\n")]
+
+        correctly_ordered = []
+
+    for i, (left, right) in enumerate(pairs):
+            if compare(left, right):
+                correctly_ordered.append(i + 1)
+
+    return sum(correctly_ordered)
+
+
+if __name__ == "__main__":
+    result = find_ordered_pairs("test.txt")
+    print(result)
+    assert result == 13
+
+    result = find_ordered_pairs("input.txt")
+    print(result)
---
+++ 13/part02.py
@@ -0,0 +1,29 @@
+from part01 import compare
+import json
+
+class Packet:
+    def __init__(self, val):
+        self.val = val
+
+    def __lt__(self, other):
+        return compare(self.val, other.val)
+
+
+def order_pairs(fn):
+    with open(fn) as f:
+        lines = [Packet(json.loads(l)) for l in f.readlines() if l.strip()]
+        lines.append(Packet([[2]]))
+        lines.append(Packet([[6]]))
+
+        lines = sorted(lines)
+        lines = [l.val for l in lines]
+
+        return (lines.index([[2]]) + 1) * (lines.index([[6]]) + 1)
+
+
+if __name__ == "__main__":
+    result = order_pairs("test.txt")
+    assert result == 140
+
+    result = order_pairs("input.txt")
+    print(result)
